8880f9aaae3be3a785352a052f59dc881c26a265
Mike Odnis <airwolf635@gmail.com>
2024-10-24 13:31:10 -0400

    Quick commit

--- week-2/src/app/convex-client-provider.tsx
+++ week-2/src/app/convex-client-provider.tsx
@@ -3,8 +3,9 @@
 import React from 'react'
 import { ConvexReactClient } from 'convex/react'
 import { ConvexProviderWithClerk } from 'convex/react-clerk'
-import { ClerkLoaded, ClerkLoading, ClerkProvider, useAuth } from '@clerk/nextjs'
+import { ClerkLoading, ClerkProvider, RedirectToSignIn, useAuth } from '@clerk/nextjs'
 import { MultisessionAppSupport } from "@clerk/clerk-react/internal";
+import { Authenticated, Unauthenticated } from 'convex/react'; 
 
 const convexClient = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL! as string);
 
@@ -22,9 +23,12 @@ export const ConvexClientProvider = ({ children }: {
           <ClerkLoading>
             <Loader />
           </ClerkLoading>
-          <ClerkLoaded>
+          <Authenticated>
             {children}
-          </ClerkLoaded>
+          </Authenticated>
+          <Unauthenticated>
+            <RedirectToSignIn />
+          </Unauthenticated>
         </ConvexProviderWithClerk>
       </MultisessionAppSupport>
     </ClerkProvider>
