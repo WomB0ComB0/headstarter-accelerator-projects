6c073f417e94fc09984de91821c6c6b77f50f154
Mike Odnis <airwolf635@gmail.com>
2024-10-24 13:50:57 -0400

    Quick commit

--- week-2/middleware.ts
+++ week-2/middleware.ts
@@ -1,10 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { useAuth } from '@clerk/nextjs';
 
 const isProtectedRoute = createRouteMatcher(["/api(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth.protect();
-});
+export default async function middleware(req: NextRequest) {
+  const auth = useAuth();
+  clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth.protect();
+  });
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
