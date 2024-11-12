e493390a1de11d555af9dbb49fe183f9471c8e4f
Mike Odnis <airwolf635@gmail.com>
2024-11-11 22:50:47 -0500

    Quick commit

--- week-3/utils.py
+++ week-3/utils.py
@@ -14,6 +14,7 @@ import logging
 from pathlib import Path
 import google.generativeai as genai
 import kagglehub
+import streamlit as st
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
